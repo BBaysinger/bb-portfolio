4254119f6b48b30d69ee9ab5ab62a33627c81435
Bradley Baysinger <bradleybaysinger@gmail.com>
2024-10-30 05:17:28 -0700

    Configs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,4 +10,11 @@ export default defineConfig({
       '@': '/src', // Set up an alias
     }
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler' // or "modern"
+      },
+    },
+  },
 })
