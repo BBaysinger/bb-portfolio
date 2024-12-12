8ca601d75ea51e9197b6b514a565a59f4887702e
Bradley Baysinger <bradleybaysinger@gmail.com>
2024-12-12 00:43:25 -0800

    Made the fluid-width mixin appear not anti-mobile-first, which it wasn't but may have seemed that way at a glance. Some CSS fixes.

--- frontend/src/utils/ScrollToHash.ts
+++ frontend/src/utils/ScrollToHash.ts
@@ -8,7 +8,7 @@ const ScrollToHash = () => {
     if (hash) {
       const element = document.querySelector(hash);
       if (element) {
-        // console.info("Scrolling to hash", hash);
+        // console.info("Scrolling to hash:", hash);
         element.scrollIntoView({ behavior: "smooth" });
       }
     }
