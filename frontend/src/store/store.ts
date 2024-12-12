4ca35cb768a7edd3a839e0ff8d8c06a81ea9a380
Bradley Baysinger <bradleybaysinger@gmail.com>
2024-12-12 00:23:36 -0800

    "@types/react-dom": "^19.0.2"

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,15 +1,15 @@
-import { createStore, combineReducers } from "redux";
+// import { createStore, combineReducers } from "redux";
 
-// Combine reducers, including the router reducer
-const rootReducer = combineReducers({
-  // router: createRouterReducer(history),
-  // Add your other reducers here
-});
+// // Combine reducers, including the router reducer
+// const rootReducer = combineReducers({
+//   // router: createRouterReducer(history),
+//   // Add your other reducers here
+// });
 
-// Create the Redux store with the router middleware
-const store = createStore(
-  rootReducer,
-  // applyMiddleware(createRouterMiddleware(history))
-);
+// // Create the Redux store with the router middleware
+// const store = createStore(
+//   rootReducer,
+//   // applyMiddleware(createRouterMiddleware(history))
+// );
 
-export { store };
+// export { store };
