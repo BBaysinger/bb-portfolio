fabaf7fadfc40525461d3edc513b9753b9ef5a5e
Bradley Baysinger <bradleybaysinger@gmail.com>
2024-12-14 02:24:23 -0800

    Yes

--- frontend/src/components/project-carousel-page/ParallaxStepCarousel.tsx
+++ frontend/src/components/project-carousel-page/ParallaxStepCarousel.tsx
@@ -45,7 +45,7 @@ const ParallaxStepCarousel: React.FC<ParallaxStepCarouselProps> = ({
     return Array.from({ length }, () => getRandomColorWithOpacity(opacity));
   }
 
-  const colorArray = generateColorArray(layer1Slides.length, 0.5);
+  const colorArray = generateColorArray(layer1Slides.length, 0.0);
 
   function handleScrollUpdate(scrollOffset: number) {
     console.log("scrollOffset:", scrollOffset);
