d5e5abe3cf57ab4f8af05a176e8844286b7c0e18
Bradley Baysinger <bradleybaysinger@gmail.com>
2024-12-18 17:21:55 -0800

    More optimizing.

--- frontend/src/components/project-carousel-page/ProjectParallaxCarousel.tsx
+++ frontend/src/components/project-carousel-page/ProjectParallaxCarousel.tsx
@@ -98,7 +98,7 @@ const ProjectParallaxCarousel: React.FC<ProjectParallaxCarouselProps> = ({
         onScrollUpdate={setMasterScrollLeft}
         onStableIndex={onStableIndex} // Detect stabilization
         onIndexUpdate={onIndexUpdate} // Detect destabilization
-        debug={true}
+        debug={false}
         wrapperClassName="bb-carousel bb-carousel-master"
         slideClassName="bb-slide-wrapper"
       />
