f48ec010caae9ddeab2095100a1938c2ae5ad817
Bradley Baysinger <bradleybaysinger@gmail.com>
2024-12-24 22:53:14 -0800

    Converting class components to FC.

--- frontend/src/components/home-page/ProjectThumbnail.tsx
+++ frontend/src/components/home-page/ProjectThumbnail.tsx
@@ -1,4 +1,4 @@
-import React, { createRef } from "react";
+import React, { forwardRef } from "react";
 import { Link } from "react-router-dom";
 
 import HoverCapabilityWatcher from "utils/HoverCapabilityWatcher";
@@ -22,49 +22,39 @@ interface ProjectThumbnailProps {
  * @since The beginning of time.
  * @version N/A
  */
-export default class ProjectThumbnail extends React.Component<ProjectThumbnailProps> {
-  private divRef = createRef<HTMLDivElement>();
-
-  getDOMNode() {
-    return this.divRef.current;
-  }
-
-  /**
-   *
-   *
-   * @returns
-   * @memberof ProjectThumbnail
-   */
-  render() {
-    const { projectId, title, clientId } = this.props;
-
+const ProjectThumbnail = forwardRef<HTMLDivElement, ProjectThumbnailProps>(
+  ({ focused, projectId, title, clientId }, ref) => {
     const style: React.CSSProperties = {
-      backgroundImage: "url('/images/thumbs/" + projectId + ".jpg')",
+      backgroundImage: `url('/images/thumbs/${projectId}.jpg')`,
     };
 
-    const focus = this.props.focused ? styles["project-thumbnail-focus"] : "";
-    const hoverEnabled = HoverCapabilityWatcher.instance.isHoverCapable
-      ? styles["hover-enabled"]
-      : "";
+    const focusClass = focused ? styles["project-thumbnail-focus"] : "";
+    const isHoverEnabledClass = () => {
+      return HoverCapabilityWatcher.instance.isHoverCapable
+        ? styles["hover-enabled"]
+        : "";
+    };
 
     return (
       <div
-        className={`${styles["project-thumbnail"]} ${focus} ${hoverEnabled}`}
+        className={`${styles["project-thumbnail"]} ${focusClass} ${isHoverEnabledClass()}`}
         style={style}
-        ref={this.divRef}
+        ref={ref}
       >
         <Link to={`/portfolio/${projectId}#project`}>
           <div className={styles["vingette"]}></div>
           <div className={styles["thumb-content"]}>
             <img
-              src={"/images/client-logos/" + clientId + ".svg"}
+              src={`/images/client-logos/${clientId}.svg`}
               className={styles["client-logo"]}
-              alt={clientId + " logo"}
+              alt={`${clientId} logo`}
             />
             <h4 className={styles["thumb-title"]}>{title}</h4>
           </div>
         </Link>
       </div>
     );
-  }
-}
+  },
+);
+
+export default ProjectThumbnail;
