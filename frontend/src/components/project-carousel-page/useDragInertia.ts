3885977dbc02124ccfe95a02a2d1e004d5424b64
Bradley Baysinger <bhbaysinger@gmail.com>
2025-02-14 20:08:25 -0800

    Using GSAP drag inertia.

--- frontend/src/components/project-carousel-page/useDragInertia.ts
+++ frontend/src/components/project-carousel-page/useDragInertia.ts
@@ -8,7 +8,9 @@ gsap.registerPlugin(Draggable, InertiaPlugin);
 
 /**
  * Custom hook to handle click/drag/inertia for a carousel component.
- * Reference: https://gsap.com/community/forums/topic/33288-gsap-observer-velocity-drag/
+ * Reference:
+ * https://gsap.com/community/forums/topic/33288-gsap-observer-velocity-drag/
+ * https://gsap.com/community/forums/topic/32443-draggable-infinite-carousel-with-snap-and-indexing/
  *
  * @author Bradley Baysinger
  * @since The beginning of time.
@@ -26,27 +28,33 @@ export const useDragInertia = (
     const scroller = scrollerRef.current;
     if (!scroller || isSlave) return;
 
+    console.log(isSlave);
     // Disable snapping when dragging starts
     const handlePress = () => setSnap("none");
 
     // Re-enable snapping when dragging ends
     // const handleRelease = () => setSnap("x mandatory");
 
+    // console.log("scroller", scroller);
+
     const draggable = Draggable.create(scroller, {
-      type: "x",
-      inertia: true,
-      edgeResistance: 0.75, // Adds slight resistance at the edges
-      bounds: scroller.parentElement || scroller, // Prevents dragging too far
-      throwProps: true, // Enables smooth inertia-based scrolling
-      cursor: "grab",
-      onPress: () => {
-        gsap.set(scroller, { cursor: "grabbing" });
-        handlePress();
-      },
-      onRelease: () => {
-        gsap.set(scroller, { cursor: "grab" });
-        // handleRelease();
-      },
+      type: "scrollLeft",
+        allowNativeTouchScrolling: true,
+        force3D: false,
+        trigger: scroller, // Ensures it uses the correct element
+        inertia: true,
+        edgeResistance: 0.75, // Adds slight resistance at the edges
+        bounds: scroller.parentElement || scroller, // Prevents dragging too far
+        throwProps: true, // Enables smooth inertia-based scrolling
+        cursor: "grab",
+        onPress: () => {
+          gsap.set(scroller, { cursor: "grabbing" });
+          handlePress();
+        },
+        onRelease: () => {
+          gsap.set(scroller, { cursor: "grab" });
+          // handleRelease();
+        },
     })[0];
 
     draggableRef.current = draggable;
