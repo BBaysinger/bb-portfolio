976567dc0d631eb10dd2a2e2c9fc757d7dd78732
Bradley Baysinger <bhbaysinger@gmail.com>
2025-02-18 23:21:42 -0800

    Getting ready to ad depth to shadows.

--- frontend/src/components/home-page/fluxel-grid/Shadow.tsx
+++ frontend/src/components/home-page/fluxel-grid/Shadow.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
+import { FluxelData } from "./Fluxel";
 import styles from "./Shadow.module.scss";
 
 interface ShadowProps {
   className?: string;
+  neighbors: FluxelData[];
 }
 
-const Shadow: React.FC<ShadowProps> = ({ className }) => {
+const Shadow: React.FC<ShadowProps> = ({ className, neighbors }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -23,11 +25,13 @@ const Shadow: React.FC<ShadowProps> = ({ className }) => {
 
       <g filter="url(#blur)">
         <rect x="50%" y="-50%" width="72" height="72" />
-        <polygon
-          x="50%"
-          y="-50%"
-          points="0 0 0 48 24 48 24 72 72 72 72 0 0 0"
-        />
+        {neighbors[0].depth && (
+          <polygon
+            x={neighbors[2].depth * 10}
+            y={neighbors[4].depth * 10}
+            points="0 0 0 48 24 48 24 72 72 72 72 0 0 0"
+          />
+        )}
       </g>
     </svg>
   );
