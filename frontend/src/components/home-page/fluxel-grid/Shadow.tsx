383e21dc523d08b0454dc03f6f1fff8bfeeeb04c
Bradley Baysinger <bhbaysinger@gmail.com>
2025-02-19 15:38:38 -0800

    Converted opacity and blur to SVG.

--- frontend/src/components/home-page/fluxel-grid/Shadow.tsx
+++ frontend/src/components/home-page/fluxel-grid/Shadow.tsx
@@ -19,12 +19,12 @@ const Shadow: React.FC<ShadowProps> = ({ className, x1, y1, x2, y2 }) => {
       viewBox="0 0 72 72"
       className={`${styles["shadow"]} ${className}`}
     >
-      <g>
+      <g filter="url(#fluxelShadowBlur)">
         <polygon
           transform={`scale(3, 3) translate(${x1}, ${y1})`}
           points="0 0 0 48 24 48 24 72 72 72 72 0 0 0"
         />
-        <rect x={x2} y={y2} width="72" height="72" />
+        {/* <rect x={x2} y={y2} width="72" height="72" /> */}
       </g>
     </svg>
   );
