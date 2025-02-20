f6160414a53e7071cbe57372ebd5aa21e92063cc
Bradley Baysinger <bhbaysinger@gmail.com>
2025-02-19 18:58:55 -0800

    Adjusting shadows. Visually back to before registration tweaks were started. Good benchmark point...

--- frontend/src/components/home-page/fluxel-grid/Shadow.tsx
+++ frontend/src/components/home-page/fluxel-grid/Shadow.tsx
@@ -12,7 +12,7 @@ interface ShadowProps {
 
 // Accounts for blur radius to keep the shadow completely out of view,
 // without hardcoding the value.
-const BLUR_OFFSET = 10;
+const BLUR_OFFSET = 0;
 
 /**
  * Registration is set to the corner of the origin of the shadows, and
@@ -34,7 +34,7 @@ const Shadow: React.FC<ShadowProps> = ({ className, x1, y1, x2, y2 }) => {
       <g filter="url(#fluxelShadowBlur)">
         <g transform={`translate(${x1}, ${y1})`}>
           <polygon
-            transform={`translate(${BLUR_OFFSET - 72}, ${-BLUR_OFFSET - 144})`}
+            transform={`translate(${BLUR_OFFSET - 34}, ${-BLUR_OFFSET - 110})`}
             points="0 0 0 108 108 108 108 216 216 216 216 0 0 0"
           />
         </g>
