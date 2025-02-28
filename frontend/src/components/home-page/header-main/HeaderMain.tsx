77582a0c40a7a83cd8f93461cd27d79abe468a1c
Bradley Baysinger <bhbaysinger@gmail.com>
2025-02-27 21:27:46 -0800

    More fixing offset issues from orientation changes.

--- frontend/src/components/home-page/header-main/HeaderMain.tsx
+++ frontend/src/components/home-page/header-main/HeaderMain.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 
 import headerLogo from "images/main-header/bb-gradient.webp";
 import BarberPole from "components/common/BarberPole";
@@ -15,14 +15,70 @@ import styles from "./HeaderMain.module.scss";
  * @version N/A
  */
 const HeaderMain: React.FC = () => {
+  const getHeight = () => document.documentElement.clientHeight;
+  const getWidth = () => document.documentElement.clientWidth;
+
   const [slingerPos, setSlingerPos] = useState<
     { x: number; y: number } | null | undefined
   >(undefined);
+  const [clientHeight, setClientHeight] = useState(getHeight());
+  const [clientWidth, setClientWidth] = useState(getWidth());
 
-  // const [scrolledToTop, setScrolledToTop] = useState<string>("scrolled-to-top");
+  // Refs to store the latest height and width
+  const clientHeightRef = useRef(clientHeight);
+  const clientWidthRef = useRef(clientWidth);
 
-  // const lastScrollPosition = useRef(0);
-  // const ticking = useRef(false);
+  useEffect(() => {
+    clientHeightRef.current = clientHeight;
+    clientWidthRef.current = clientWidth;
+  }, [clientHeight, clientWidth]);
+
+  const updateClientDimensions = useCallback(() => {
+    const h = getHeight();
+    const w = getWidth();
+
+    if (clientHeightRef.current !== h) setClientHeight(h);
+    if (clientWidthRef.current !== w) setClientWidth(w);
+  }, []);
+
+  useEffect(() => {
+    updateClientDimensions();
+
+    const handleResize = () => {
+      requestAnimationFrame(updateClientDimensions);
+    };
+
+    window.addEventListener("resize", handleResize);
+    window.addEventListener("orientationchange", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      window.removeEventListener("orientationchange", handleResize);
+    };
+  }, [updateClientDimensions]);
+
+  const onSlingerDrag = useCallback(
+    (x: number, y: number, e: MouseEvent | TouchEvent) => {
+      if (e.type === "touchmove") {
+        const longestSide = Math.max(
+          clientWidthRef.current,
+          clientHeightRef.current,
+        );
+        const offsetX = x + (longestSide - clientWidthRef.current) / 2;
+        const offsetY = y + (longestSide - clientHeightRef.current) / 2;
+        setSlingerPos({ x: offsetX, y: offsetY });
+      }
+    },
+    [],
+  );
+
+  const onSlingerDragEnd = useCallback(
+    (_x: number, _y: number, e: MouseEvent | TouchEvent) => {
+      if (e.type === "touchend") {
+        setSlingerPos(null);
+      }
+    },
+    [],
+  );
 
   const quotes = [
     "Interactivity is not about clicking, tapping, or swiping. It's about engagement — an invitation to explore, respond, and shape the experience.",
@@ -44,105 +100,10 @@ const HeaderMain: React.FC = () => {
     "Interfaces should be designed with curiosity in mind. If the user wants to explore, let them — and reward them for doing so.",
   ];
 
-  const getHeight = () => {
-    // This is the only way to get the 'short' height of the mobile viewport.
-    // That is, the height before the address bar is hidden from scrolling down.
-    // 'dvh' here can't be used because it causes a layout shift.
-    return document.documentElement.clientHeight;
-  };
-
-  const getWidth = () => {
-    return document.documentElement.clientWidth;
-  };
-
-  const [clientHeight, setClientHeight] = useState(getHeight());
-  const [clientWidth, setClientWidth] = useState(getWidth());
-
-  useEffect(() => {
-    const updateClientDimensions = () => {
-      const h = getHeight();
-      const w = getWidth();
-
-      if (h !== clientHeight || w !== clientWidth) {
-        // This is the only way to get the 'short' height of the mobile viewport.
-        // That is, the height before the address bar is hidden from scrolling down.
-        // 'dvh' here can't be used because it causes a layout shift.
-        setClientHeight(h);
-        setClientWidth(w);
-      }
-    };
-
-    updateClientDimensions();
-
-    window.addEventListener("resize", updateClientDimensions);
-    window.addEventListener("orientationchange", updateClientDimensions);
-
-    return () => {
-      window.removeEventListener("resize", updateClientDimensions);
-      window.removeEventListener("orientationchange", updateClientDimensions);
-    };
-  }, [clientHeight, clientWidth]);
-
-  // useEffect(() => {
-  //  KEEP: This was for the view portfolio CTA that's probably coming back.
-  //   const handleEvent = () => {
-  //     if (!ticking.current) {
-  //       ticking.current = true;
-
-  //       requestAnimationFrame(() => {
-  //         const currentScrollPosition = window.scrollY;
-
-  //         const newScrolledToTop =
-  //           currentScrollPosition > 0 ? "" : styles["scrolled-to-top"];
-  //         if (newScrolledToTop !== scrolledToTop) {
-  //           setScrolledToTop(newScrolledToTop);
-  //         }
-
-  //         lastScrollPosition.current = currentScrollPosition;
-  //         ticking.current = false;
-  //       });
-  //     }
-  //   };
-
-  //   window.addEventListener("scroll", handleEvent);
-  //   window.addEventListener("resize", handleEvent);
-  //   window.addEventListener("orientationchange", handleEvent);
-
-  //   return () => {
-  //     window.removeEventListener("scroll", handleEvent);
-  //     window.removeEventListener("resize", handleEvent);
-  //     window.removeEventListener("orientationchange", handleEvent);
-  //   };
-  // }, [scrolledToTop]);
-
-  const onSlingerDrag = useCallback(
-    (x: number, y: number, e: MouseEvent | TouchEvent) => {
-      if (e.type === "touchmove") {
-        const longestSide = Math.max(clientWidth, clientHeight);
-        const offsetX = x + (longestSide - clientWidth) / 2;
-        const offsetY = y + (longestSide - clientHeight) / 2;
-        setSlingerPos({ x: offsetX, y: offsetY });
-      }
-    },
-    [],
-  );
-
-  const onSlingerDragEnd = useCallback(
-    (_x: number, _y: number, e: MouseEvent | TouchEvent) => {
-      if (e.type === "touchend") {
-        setSlingerPos(null);
-      }
-    },
-    [],
-  );
-
   return (
     <header
-      id={"headerMain"}
-      className={
-        `${styles["header-main"]} ${styles["header"]} header-main` // +
-        // `${scrolledToTop ? styles["scrolled-to-top"] : ""}`
-      }
+      id="headerMain"
+      className={`${styles["header-main"]} ${styles["header"]} header-main`}
       style={{ minHeight: `${clientHeight}px` }}
     >
       <div className={styles["fluxel-wrapper"]}>
@@ -178,16 +139,8 @@ const HeaderMain: React.FC = () => {
           Interactive&nbsp;Web <span className={styles["bull"]}>&bull;</span>{" "}
           <span className={styles["nobr"]}>Front-end Developer</span>
         </h5>
-        {/* <a href="#list" className={styles["view-portfolio"]}>
-          View Portfolio
-        </a> */}
       </div>
       <ParagraphAnimator paragraphs={quotes} className={styles["message"]} />
-      {/* {true && (
-        <div className={styles["debug"]}>
-          {clientWidth}, {clientHeight}
-        </div>
-      )} */}
     </header>
   );
 };
