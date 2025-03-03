a0f64010f358d488ea42f895986f6b741756c327
Bradley Baysinger <bhbaysinger@gmail.com>
2025-03-02 21:30:22 -0800

    Added road sign.

--- frontend/src/components/home-page/Greeting.tsx
+++ frontend/src/components/home-page/Greeting.tsx
@@ -10,31 +10,47 @@ const Greeting: React.FC = () => {
     <div className={styles["greeting"]}>
       <div className={styles["greeting-wrapper"]}>
         <div className="container">
-          <p>
-            Hi, I'm Bradley. I'm a front-end web developer with a background in
-            design, specializing in building high-quality, interactive web
-            experiences. My current expertise lies in TypeScript, JavaScript,
-            and modern development practices, with a focus on performance,
-            scalability, and usability.
-          </p>
+          <div className="row">
+            <div
+              className={`${styles["sign-wrapper"]} col-xs-12 col-sm-12 col-md-3 col-lg-3`}
+            >
+              <img
+                src="/images/home/road-sign.webp"
+                className={styles["road-sign"]}
+                alt="road sign"
+              />
+            </div>
 
-          <p>
-            With years in crafting digital experiences, I take a thoughtful
-            approach to every project—balancing aesthetics with functionality to
-            create seamless user experiences. Whether it's a dynamic web
-            application, a pixel-perfect UI, or a custom-built component, I
-            strive to write clean, maintainable code that brings designs to
-            life.
-          </p>
+            <div
+              className={`${styles["text"]} col-xs-12 col-sm-12 col-md-9 col-lg-9`}
+            >
+              <p>
+                Hi, I'm Bradley. I'm a front-end web developer with a background
+                in design, specializing in building high-quality, interactive
+                web experiences. My current expertise lies in TypeScript,
+                JavaScript, and modern development practices, with a focus on
+                performance, scalability, and usability.
+              </p>
 
-          <p>
-            My portfolio includes both modern projects and select legacy pieces,
-            highlighting my unique history, adaptability, and attention to
-            detail. These showcase not only my technical evolution but also my
-            ability to refine and polish digital experiences across different
-            eras of web development. Feel free to browse my work, and if you'd
-            like to connect, let's talk!
-          </p>
+              <p>
+                With years in crafting digital experiences, I take a thoughtful
+                approach to every project—balancing aesthetics with
+                functionality to create seamless user experiences. Whether it's
+                a dynamic web application, a pixel-perfect UI, or a custom-built
+                component, I strive to write clean, maintainable code that
+                brings designs to life.
+              </p>
+
+              <p>
+                My portfolio includes both modern projects and select legacy
+                pieces, highlighting my unique history, adaptability, and
+                attention to detail. These showcase not only my technical
+                evolution but also my ability to refine and polish digital
+                experiences across different eras of web development. Feel free
+                to browse my work, and if you'd like to connect, let's talk!
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
