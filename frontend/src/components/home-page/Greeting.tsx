ee6e3d2eb7faeb63d386bad727f5fecc87210083
Bradley Baysinger <bhbaysinger@gmail.com>
2025-03-03 17:42:21 -0800

    Styling greeting. Minor other styling.

--- frontend/src/components/home-page/Greeting.tsx
+++ frontend/src/components/home-page/Greeting.tsx
@@ -26,35 +26,36 @@ const Greeting: React.FC = () => {
               />
             </div>
 
-            <div
-              className={`${styles["text"]} col-xs-12 col-sm-12 col-md-8 col-lg-8`}
-            >
-              <p>
-                Hi, I'm Bradley. Welcome to my portfolio site. I'm a front-end
-                web developer with a background in design, specializing in
-                building high-quality, interactive web experiences. My current
-                expertise lies in TypeScript, JavaScript, and modern development
-                practices, with a focus on performance, scalability, and
-                usability.
-              </p>
+            <div className={`col-xs-12 col-sm-12 col-md-8 col-lg-8`}>
+              <div className={`${styles["text"]}`}>
+                <p>
+                  Hi, I'm Bradley. Welcome to my portfolio site. I'm a front-end
+                  web developer with a background in design, specializing in
+                  building high-quality, interactive web experiences. My current
+                  expertise lies in TypeScript, JavaScript, and modern
+                  development practices, with a focus on performance,
+                  scalability, and usability.
+                </p>
 
-              <p>
+                {/* <p>
                 With years in crafting digital experiences, I take a thoughtful
                 approach to every project—balancing aesthetics with
                 functionality to create seamless user experiences. Whether it's
                 a dynamic web application, a pixel-perfect UI, or a custom-built
                 component, I strive to write clean, maintainable code that
                 brings designs to life.
-              </p>
+              </p> */}
 
-              <p>
-                My portfolio includes both modern projects and select legacy
-                pieces, highlighting my unique history, adaptability, and
-                attention to detail. These showcase not only my technical
-                evolution but also my ability to refine and polish digital
-                experiences across different eras of web development. Feel free
-                to browse my work, and if you'd like to connect, let's talk!
-              </p>
+                <p>
+                  My portfolio includes both modern projects and select legacy
+                  pieces, highlighting my unique history, adaptability, and
+                  attention to detail. These showcase not only my technical
+                  evolution but also my ability to refine and polish digital
+                  experiences across different eras of web development. Feel
+                  free to browse my work, and if you'd like to connect, let's
+                  talk!
+                </p>
+              </div>
             </div>
           </div>
         </div>
