3347472308e49e8dcd177b2c84da9f209e9ee181
Bradley Baysinger <bhbaysinger@gmail.com>
2025-03-04 23:22:47 -0800

    Logout button.

---
+++ frontend/src/components/common/LogoutButton.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import styles from "./LogoutButton.module.scss";
+
+/**
+ * @author Bradley Baysinger
+ * @since The beginning of time.
+ * @version N/A
+ */
+const LogoutButton: React.FC = () => {
+  const isAuthenticated = sessionStorage.getItem("isLoggedIn") === "true";
+
+  if (!isAuthenticated) return null; // Hide button if user is logged out
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("isLoggedIn");
+    window.location.href = "/login";
+  };
+
+  return (
+    <a
+      href="javascript:void(0)"
+      className={`${styles["logout"]} logout`}
+      onClick={handleLogout}
+    >
+      <img
+        src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+        alt="Logout"
+      />
+    </a>
+  );
+};
+
+export default LogoutButton;
