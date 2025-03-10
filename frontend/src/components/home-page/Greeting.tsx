f39a38d3e54a2d8e63614a392a98b9fefe097f40
Bradley Baysinger <bhbaysinger@gmail.com>
2025-03-09 22:35:16 -0700

    Added orange and yellow into Hello section.

--- frontend/src/components/home-page/Greeting.tsx
+++ frontend/src/components/home-page/Greeting.tsx
@@ -13,11 +13,9 @@ const Greeting: React.FC = () => {
   return (
     <div id="hello" className={styles["greeting"]}>
       <div className={styles["greeting-wrapper"]}>
-        <div className="container">
-          <div className="row">
-            <div
-              className={`${styles["sign-wrapper"]} col-xs-12 col-sm-12 col-md-4 col-lg-4`}
-            >
+        <div>
+          <div>
+            <div className={`${styles["sign-wrapper"]}`}>
               <img
                 ref={addToRefs}
                 src="/images/home/road-sign-mobile.webp"
@@ -32,18 +30,19 @@ const Greeting: React.FC = () => {
               />
             </div>
 
-            <div className={`col-xs-12 col-sm-12 col-md-8 col-lg-8`}>
+            <div className={``}>
               <div className={`${styles["text"]}`}>
-                <p ref={addToRefs}>
-                  Hi, I'm Bradley. Welcome to my portfolio site. I'm a front-end
-                  web developer with a background in design, specializing in
-                  building high-quality, interactive web experiences. My current
-                  expertise lies in TypeScript, JavaScript, and modern
-                  development practices, with a focus on performance,
-                  scalability, and usability.
-                </p>
+                <div ref={addToRefs}>
+                  <p>
+                    Hi, I'm Bradley. Welcome to my portfolio site. I'm a
+                    front-end web developer with a background in design,
+                    specializing in building high-quality, interactive web
+                    experiences. My current expertise lies in TypeScript,
+                    JavaScript, and modern development practices, with a focus
+                    on performance, scalability, and usability.
+                  </p>
 
-                {/* <p>
+                  {/* <p>
                 With years in crafting digital experiences, I take a thoughtful
                 approach to every project—balancing aesthetics with
                 functionality to create seamless user experiences. Whether it's
@@ -51,17 +50,18 @@ const Greeting: React.FC = () => {
                 component, I strive to write clean, maintainable code that
                 brings designs to life.
                 </p> */}
-
-                <p ref={addToRefs}>
-                  My portfolio includes both modern projects and select legacy
-                  pieces, highlighting my unique history, adaptability, and
-                  attention to detail. These showcase not only my technical
-                  evolution but also my ability to refine and polish digital
-                  experiences across different eras of web development. Feel
-                  free to browse my work, and if you'd like to connect, let's
-                  talk!
-                </p>
-
+                </div>
+                <div ref={addToRefs}>
+                  <p>
+                    My portfolio includes both modern projects and select legacy
+                    pieces, highlighting my unique history, adaptability, and
+                    attention to detail. These showcase not only my technical
+                    evolution but also my ability to refine and polish digital
+                    experiences across different eras of web development. Feel
+                    free to browse my work, and if you'd like to connect, let's
+                    talk!
+                  </p>
+                </div>
                 <a ref={addToRefs} href="#list">
                   3. Take a look!
                 </a>
