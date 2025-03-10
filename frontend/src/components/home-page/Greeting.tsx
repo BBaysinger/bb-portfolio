25b8ba78ab5ba59b06e29799751291c015d3bc07
Bradley Baysinger <bhbaysinger@gmail.com>
2025-03-09 23:09:37 -0700

    Scaling the hello section.

--- frontend/src/components/home-page/Greeting.tsx
+++ frontend/src/components/home-page/Greeting.tsx
@@ -13,36 +13,34 @@ const Greeting: React.FC = () => {
   return (
     <div id="hello" className={styles["greeting"]}>
       <div className={styles["greeting-wrapper"]}>
-        <div>
-          <div>
-            <div className={`${styles["sign-wrapper"]}`}>
-              <img
-                ref={addToRefs}
-                src="/images/home/road-sign-mobile.webp"
-                className={styles["road-sign-mobile"]}
-                alt="road sign"
-              />
-              <img
-                ref={addToRefs}
-                src="/images/home/road-sign-desktop.webp"
-                className={styles["road-sign-desktop"]}
-                alt="road sign"
-              />
-            </div>
+        <div className={`${styles["sign-wrapper"]}`}>
+          <img
+            ref={addToRefs}
+            src="/images/home/road-sign-mobile.webp"
+            className={styles["road-sign-mobile"]}
+            alt="road sign"
+          />
+          <img
+            ref={addToRefs}
+            src="/images/home/road-sign-desktop.webp"
+            className={styles["road-sign-desktop"]}
+            alt="road sign"
+          />
+        </div>
 
-            <div className={``}>
-              <div className={`${styles["text"]}`}>
-                <div ref={addToRefs}>
-                  <p>
-                    Hi, I'm Bradley. Welcome to my portfolio site. I'm a
-                    front-end web developer with a background in design,
-                    specializing in building high-quality, interactive web
-                    experiences. My current expertise lies in TypeScript,
-                    JavaScript, and modern development practices, with a focus
-                    on performance, scalability, and usability.
-                  </p>
+        <div className={``}>
+          <div className={`${styles["text"]}`}>
+            <div ref={addToRefs}>
+              <p>
+                Hi, I'm Bradley. Welcome to my portfolio site. I'm a front-end
+                web developer with a background in design, specializing in
+                building high-quality, interactive web experiences. My current
+                expertise lies in TypeScript, JavaScript, and modern development
+                practices, with a focus on performance, scalability, and
+                usability.
+              </p>
 
-                  {/* <p>
+              {/* <p>
                 With years in crafting digital experiences, I take a thoughtful
                 approach to every project—balancing aesthetics with
                 functionality to create seamless user experiences. Whether it's
@@ -50,23 +48,20 @@ const Greeting: React.FC = () => {
                 component, I strive to write clean, maintainable code that
                 brings designs to life.
                 </p> */}
-                </div>
-                <div ref={addToRefs}>
-                  <p>
-                    My portfolio includes both modern projects and select legacy
-                    pieces, highlighting my unique history, adaptability, and
-                    attention to detail. These showcase not only my technical
-                    evolution but also my ability to refine and polish digital
-                    experiences across different eras of web development. Feel
-                    free to browse my work, and if you'd like to connect, let's
-                    talk!
-                  </p>
-                </div>
-                <a ref={addToRefs} href="#list">
-                  3. Take a look!
-                </a>
-              </div>
             </div>
+            <div ref={addToRefs}>
+              <p>
+                My portfolio includes both modern projects and select legacy
+                pieces, highlighting my unique history, adaptability, and
+                attention to detail. These showcase not only my technical
+                evolution but also my ability to refine and polish digital
+                experiences across different eras of web development. Feel free
+                to browse my work, and if you'd like to connect, let's talk!
+              </p>
+            </div>
+            <a ref={addToRefs} href="#list">
+              3. Take a look!
+            </a>
           </div>
         </div>
       </div>
