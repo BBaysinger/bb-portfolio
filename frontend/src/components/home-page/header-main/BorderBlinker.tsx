29a35613011f696908219c86114abe5f2d23ac88
Bradley Baysinger <bhbaysinger@gmail.com>
2025-04-14 19:16:28 -0700

    Added border blinker.

---
+++ frontend/src/components/home-page/header-main/BorderBlinker.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import styles from "./BorderBlinker.module.scss";
+
+/**
+ * Border Blinker
+ *
+ * @author Bradley Baysinger
+ * @since The beginning of time.
+ * @version N/A
+ */
+const BorderBlinker: React.FC = () => {
+  return <div className={styles.borderBlinker}></div>;
+};
+
+export default BorderBlinker;
