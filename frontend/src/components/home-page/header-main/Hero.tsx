730e95099378acbc27f419445858ee4152459f95
Bradley Baysinger <bhbaysinger@gmail.com>
2025-05-06 18:07:14 -0700

    Fuck you.

--- frontend/src/components/home-page/header-main/Hero.tsx
+++ frontend/src/components/home-page/header-main/Hero.tsx
@@ -197,7 +197,7 @@ const Hero: React.FC = () => {
 
         <ParagraphAnimator
           introMessage={`Welcome! This is a tactile UI experiment.
-           Grab the floating green orb, then give it a toss.
+           Grab the floating orb, then give it a toss.
            I double dare you.
           `}
           paragraphs={quotes}
