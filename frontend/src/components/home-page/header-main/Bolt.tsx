d3347e3b589ce5ef34f3f934d9f2b71b3030a8da
Bradley Baysinger <bhbaysinger@gmail.com>
2025-05-17 02:24:40 -0700

    Added bolt and lightning circle.

---
+++ frontend/src/components/home-page/header-main/Bolt.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import styles from "./Bolt.module.scss";
+
+/**
+ * Lightning Bolt Component
+ *
+ * @component
+ */
+const Bolt: React.FC = ({}) => {
+  return <div className={styles.bolt}></div>;
+};
+
+export default Bolt;
