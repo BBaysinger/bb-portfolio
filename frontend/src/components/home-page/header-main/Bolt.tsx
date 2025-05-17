c4f6062c5931f21fac26294708546a7d79135564
Bradley Baysinger <bhbaysinger@gmail.com>
2025-05-17 03:17:17 -0700

    added images

--- frontend/src/components/home-page/header-main/Bolt.tsx
+++ frontend/src/components/home-page/header-main/Bolt.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import SpriteSheetPlayer from "components/common/SpriteSheetPlayer";
 import styles from "./Bolt.module.scss";
 
 /**
@@ -8,7 +9,13 @@ import styles from "./Bolt.module.scss";
  * @component
  */
 const Bolt: React.FC = ({}) => {
-  return <div className={styles.bolt}></div>;
+  const src = "/spritesheets/blue-lightning_Layer-Comp-_w16h12f6.webp";
+  return (
+    <>
+      <SpriteSheetPlayer src={src} fps={30} loops={0} />
+      <div className={styles.bolt} />
+    </>
+  );
 };
 
 export default Bolt;
