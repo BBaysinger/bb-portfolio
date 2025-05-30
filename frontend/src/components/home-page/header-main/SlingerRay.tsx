f476b452b9c22eb656f21303b92c3df6ca9342e0
Bradley Baysinger <bhbaysinger@gmail.com>
2025-05-29 23:39:10 -0700

    Sized energy rays.

--- frontend/src/components/home-page/header-main/SlingerRay.tsx
+++ frontend/src/components/home-page/header-main/SlingerRay.tsx
@@ -17,11 +17,11 @@ const SlingerRay: React.FC<SlingerRayProps> = ({
   const [lightningFrame, setLightningFrame] = useState<number | null>(-1); // hidden
   const [energyBarsFrame, setEnergyBarsFrame] = useState<number | null>(-1); // hidden
 
-const onBarsEnded = () => {
-  setEnergyBarsFrame(-1); // hide energy bars
-  setLightningFrame(Math.random()); // set to a unique value to force useEffect
-  requestAnimationFrame(() => setLightningFrame(null)); // release control to start animation
-};
+  const onBarsEnded = () => {
+    setEnergyBarsFrame(-1); // hide energy bars
+    setLightningFrame(Math.random()); // set to a unique value to force useEffect
+    requestAnimationFrame(() => setLightningFrame(null)); // release control to start animation
+  };
 
   useEffect(() => {
     if (isActive) {
