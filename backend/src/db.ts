0bc02586ea9e3f9445f73fd1faa2655456aa3cba
Bradley Baysinger <bhbaysinger@gmail.com>
2025-06-22 22:06:26 -0700

    Prerequisites for Payload. Set up Prettier.

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,14 +1,16 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
 
 dotenv.config();
+
+console.log("MONGODB_URI:", process.env.MONGODB_URI);
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI!);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error('MongoDB connection error:', error);
+    console.error("MongoDB connection error:", error);
     process.exit(1);
   }
 };
