179e351b7eeffc2db1ccd9e86268d96a7e6ca1d1
Bradley Baysinger <bhbaysinger@gmail.com>
2025-06-30 00:40:53 -0700

    Moved payload.config.

--- backend/payload.config.ts
+++ backend/payload.config.ts
@@ -1,12 +1,20 @@
-// backend/payload.config.ts
-import { buildConfig } from "payload/config";
+import { buildConfig } from "payload";
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import * as dotenv from "dotenv";
+
+import Projects from "./src/collections/Projects";
+import Users from "./src/collections/Users";
+
+dotenv.config();
 
 export default buildConfig({
   serverURL: "http://localhost:3000",
   admin: {
     user: "users",
   },
-  collections: [
-    // You'll add your first collection here shortly
-  ],
+  collections: [Projects, Users],
+  db: mongooseAdapter({
+    url: process.env.MONGODB_URI || "mongodb://localhost/portfolio",
+  }),
+  secret: process.env.PAYLOAD_SECRET || "dev-secret",
 });
