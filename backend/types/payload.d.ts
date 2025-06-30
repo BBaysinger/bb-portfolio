f9f8e7fc7ae4574cba6ab9dbb1e31fdda4770f44
Bradley Baysinger <bhbaysinger@gmail.com>
2025-06-30 02:07:46 -0700

    Generated from create-payload-app, since ChatGPT couldn't figure shit out.

--- backend/types/payload.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "payload/types" {
-  export type CollectionConfig = any;
-  export type BuildConfig = any;
-}
-
-declare module "payload/config" {
-  import type { BuildConfig } from "payload/types";
-  export function buildConfig(config: BuildConfig): BuildConfig;
-}
