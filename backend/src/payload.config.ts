0569ae6a1f83d7fe1d66b30d60779f33220308eb
Bradley Baysinger <bhbaysinger@gmail.com>
2025-06-30 00:17:16 -0700

    Added Users collection. Backend runs now!

--- backend/src/payload.config.ts
+++ backend/src/payload.config.ts
@@ -1,13 +1,18 @@
 import { buildConfig } from "payload";
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
-import Projects from "collections/Projects";
+import * as dotenv from "dotenv";
+
+import Projects from "./collections/Projects";
+import Users from "./collections/Users";
+
+dotenv.config();
 
 export default buildConfig({
   serverURL: "http://localhost:3000",
   admin: {
     user: "users",
   },
-  collections: [Projects],
+  collections: [Projects, Users],
   db: mongooseAdapter({
     url: process.env.MONGODB_URI || "mongodb://localhost/portfolio",
   }),
