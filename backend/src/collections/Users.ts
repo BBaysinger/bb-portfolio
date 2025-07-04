0a0b12ee7d6789c19bb2a60ae878770dc18bb9e3
Bradley Baysinger <bhbaysinger@gmail.com>
2025-07-03 23:35:17 -0700

    New schemas. .prettierignore in frontend and backend.

--- backend/src/collections/Users.ts
+++ backend/src/collections/Users.ts
@@ -1,13 +1,15 @@
-import type { CollectionConfig } from 'payload'
+import { CollectionConfig } from 'payload'
 
 export const Users: CollectionConfig = {
   slug: 'users',
-  admin: {
-    useAsTitle: 'email',
-  },
   auth: true,
   fields: [
-    // Email added by default
-    // Add more fields as needed
+    {
+      name: 'role',
+      type: 'select',
+      options: ['admin', 'user'],
+      defaultValue: 'user',
+      required: true,
+    },
   ],
 }
