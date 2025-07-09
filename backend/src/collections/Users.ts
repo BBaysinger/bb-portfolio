d16736984bb10f02c1c9ba781e6396f6053b3fdd
Bradley Baysinger <bhbaysinger@gmail.com>
2025-07-09 16:46:49 -0700

    User schema.

--- backend/src/collections/Users.ts
+++ backend/src/collections/Users.ts
@@ -3,6 +3,20 @@ import { CollectionConfig } from 'payload'
 export const Users: CollectionConfig = {
   slug: 'users',
   auth: true,
+  admin: {
+    useAsTitle: 'fullName',
+    defaultColumns: ['fullName', 'email', 'role'], // List view columns
+  },
+  hooks: {
+    beforeChange: [
+      ({ data }) => {
+        if (data?.firstName && data?.lastName && data?.email) {
+          data.fullName = `${data.firstName} ${data.lastName} <${data.email}>`
+        }
+        return data
+      },
+    ],
+  },
   fields: [
     {
       name: 'role',
@@ -10,6 +24,24 @@ export const Users: CollectionConfig = {
       options: ['admin', 'user'],
       defaultValue: 'user',
       required: true,
+    },
+    {
+      name: 'firstName',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'lastName',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'fullName',
+      type: 'text',
+      admin: {
+        readOnly: true,
+        position: 'sidebar', // optional
+      },
     },
   ],
 }
