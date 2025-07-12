c7fceb28fd3ec05b78cbad3192feb084ff35d2cc
Bradley Baysinger <bhbaysinger@gmail.com>
2025-07-11 20:28:49 -0700

    More migrations!

--- frontend/src/components/layout/FootGreet.tsx
+++ frontend/src/components/layout/FootGreet.tsx
@@ -1,8 +1,12 @@
+import Image from "next/image";
+
 import useTimeOfDay from "@/hooks/useTimeOfDay";
+
 import styles from "./FootGreet.module.scss";
 
 /**
- *
+ * The greeting in the footer.
+ * Time of day included for a subtle dynamic touch.
  *
  * @author Bradley Baysinger
  * @since The beginning of time.
@@ -19,15 +23,16 @@ const FootGreet: React.FC<{ className: string }> = ({ className }) => {
           className={`img-responsive ${styles.footerPhoto}`}
           alt="Bradley's face"
         />
-        Good {currentTimeOfDay}, and thanks for stopping by! I'll be honest—this
-        space is an experiment in progress, and I hope it always stays that way.
-        Some things might seem a bit mysterious, but give it time and check back
-        later!
+        Good {currentTimeOfDay}, and thanks for stopping by! I&apos;ll be
+        honest—this space is an experiment in progress, and I hope it always
+        stays that way. Some things might seem a bit mysterious, but give it
+        time and check back later!
       </p>
       <p>
-        Anyhow, I'm always looking to collaborate with forward-thinking teams
-        who value engaging digital experiences. I look forward to connecting and
-        exploring how I can bring unique interactive work to your organization.
+        Anyhow, I&apos;m always looking to collaborate with forward-thinking
+        teams who value engaging digital experiences. I look forward to
+        connecting and exploring how I can bring unique interactive work to your
+        organization.
       </p>
     </>
   );
