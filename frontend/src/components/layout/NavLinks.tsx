82c34fb73fa9e30a13c5d07e6eb94d36eb6763ec
Bradley Baysinger <bhbaysinger@gmail.com>
2025-07-11 20:28:49 -0700

    More migrations!

--- frontend/src/components/layout/NavLinks.tsx
+++ frontend/src/components/layout/NavLinks.tsx
@@ -1,11 +1,14 @@
-import React from "react";
-import Link from "next/link";
-import type { LinkRenderProps } from "react-router-dom";
+"use client";
 
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+import LogoutButton from "@/components/common/LogoutButton";
 import { useAuth } from "@/context/AuthContext";
 import MiscUtils from "@/utils/MiscUtils";
+
 import styles from "./Links.module.scss";
-import LogoutButton from "@/components/common/LogoutButton";
 
 interface LinksProps {
   onClick?: () => void;
@@ -20,66 +23,49 @@ interface LinksProps {
  * @version N/A
  */
 const Links: React.FC<LinksProps> = ({ onClick, className }) => {
+  const pathname = usePathname();
   const { isLoggedIn } = useAuth();
 
+  const isActive = (href: string): string => {
+    return MiscUtils.isActiveOrAlt(pathname === href, href, styles.active);
+  };
+
   return (
-    <ul onClick={onClick} className={`${styles.navLinks} ${className}`}>
-      {/* {isLoggedIn && ( */}
-      <>
-        <li>
-          <Link
-            to="/#top"
-            className={({ isActive }: LinkRenderProps) =>
-              MiscUtils.isActiveOrAlt(isActive, "/#top", styles.active)
-            }
-          >
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/portfolio#list"
-            className={({ isActive }: LinkRenderProps) =>
-              MiscUtils.isActiveOrAlt(
-                isActive,
-                "/portfolio#list",
-                styles.active,
-              )
-            }
-          >
-            Portfolio
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/cv#top"
-            className={({ isActive }: LinkRenderProps) =>
-              isActive ? styles.active : ""
-            }
-          >
-            CV
-          </Link>
-        </li>
-      </>
-      {/* )} */}
+    <ul onClick={onClick} className={`${styles.navLinks} ${className ?? ""}`}>
+      <li>
+        <Link href="/#top" className={isActive("/#top")}>
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link href="/portfolio#list" className={isActive("/portfolio#list")}>
+          Portfolio
+        </Link>
+      </li>
       <li>
         <Link
-          to="/contact#top"
-          className={({ isActive }: LinkRenderProps) =>
-            isActive ? styles.active : ""
-          }
+          href="/cv#top"
+          className={pathname === "/cv" ? styles.active : ""}
+        >
+          CV
+        </Link>
+      </li>
+      <li>
+        <Link
+          href="/contact#top"
+          className={pathname === "/contact" ? styles.active : ""}
         >
           Contact
         </Link>
       </li>
-      {isLoggedIn && <LogoutButton className={styles.logout} />}
-      {!isLoggedIn && (
+
+      {isLoggedIn ? (
+        <LogoutButton className={styles.logout} />
+      ) : (
         <li className={styles.login}>
           <Link
-            to="/login#top"
-            className={({ isActive }: LinkRenderProps) =>
-              isActive ? styles.active : ""
-            }
+            href="/login#top"
+            className={pathname === "/login" ? styles.active : ""}
           >
             Login
           </Link>
