56a21f8b814865c7775798ef989417c6f368a373
Bradley Baysinger <bhbaysinger@gmail.com>
2025-07-27 21:43:07 -0700

    Introduced layered carousel component and it's parent view/config component.

---
+++ frontend/src/components/project-carousel-page/ProjectCarouselView.tsx
@@ -0,0 +1,81 @@
+import React, { useMemo } from "react";
+
+import ProjectData from "@/data/ProjectData";
+
+import { SourceType, DirectionType } from "./CarouselTypes";
+import DeviceDisplay, { DeviceTypes } from "./DeviceDisplay";
+import LayeredCarouselManager, {
+  CarouselLayerConfig,
+  LayeredCarouselManagerRef,
+} from "./LayeredCarouselManager";
+import styles from "./ProjectClientPage.module.scss";
+
+const ProjectCarouselView: React.FC<{
+  projectId: string;
+  initialIndex: number;
+  refObj: React.RefObject<LayeredCarouselManagerRef>;
+  onStabilizationUpdate: (
+    index: number,
+    source: SourceType,
+    direction: DirectionType
+  ) => void;
+}> = ({ initialIndex, refObj, onStabilizationUpdate }) => {
+  const laptopSlides = useMemo(
+    () =>
+      ProjectData.activeProjects.map((project) => (
+        <DeviceDisplay
+          deviceType={DeviceTypes.LAPTOP}
+          id={project.id}
+          key={project.id}
+        />
+      )),
+    []
+  );
+
+  const phoneSlides = useMemo(
+    () =>
+      ProjectData.activeProjects.map((project) => (
+        <DeviceDisplay
+          deviceType={DeviceTypes.PHONE}
+          mobileStatus={project.mobileStatus}
+          id={project.id}
+          key={project.id}
+        />
+      )),
+    []
+  );
+
+  const layers: CarouselLayerConfig[] = [
+    {
+      id: "master",
+      spacing: 720,
+      slides: laptopSlides.map(() => null),
+      type: "master",
+    },
+    {
+      id: "laptops",
+      spacing: 693,
+      slides: laptopSlides,
+      type: "slave",
+    },
+    {
+      id: "phones",
+      spacing: 900,
+      slides: phoneSlides,
+      type: "slave",
+    },
+  ];
+
+  return (
+    <LayeredCarouselManager
+      ref={refObj}
+      prefix="bb-"
+      styles={styles}
+      layers={layers}
+      initialIndex={initialIndex}
+      onStabilizationUpdate={onStabilizationUpdate}
+    />
+  );
+};
+
+export default ProjectCarouselView;
