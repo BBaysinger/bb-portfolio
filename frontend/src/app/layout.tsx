2e104b3f428b195e2467933205e06f545f355e1c
Bradley Baysinger <bhbaysinger@gmail.com>
2025-10-10 07:16:01 -0700

    Fonts fix for iOS Sarari.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,9 @@
 import { ReactNode } from "react";
 
+import { roboto } from "@/fonts";
+
 import { ClientLayoutShell } from "./ClientLayoutShell";
 import { AppProviders } from "./providers/AppProviders";
-import { roboto } from "@/fonts";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/styles.scss";
@@ -13,10 +14,14 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <head>
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link 
-          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" 
-          rel="stylesheet" 
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
         />
       </head>
       <body className={roboto.className}>
