837ef4e4843626f2062a8a114d7a0e5c4fd0b443
Bradley Baysinger <bhbaysinger@gmail.com>
2025-11-11 08:53:58 -0800

    Toss tooltip stubbed in and partially animated. Fixed registration of orb attachments to center of orb.

--- frontend/src/components/home-page/header-main/OrbThrowTooltip.tsx
+++ frontend/src/components/home-page/header-main/OrbThrowTooltip.tsx
@@ -1,4 +1,4 @@
-import { clsx } from "clsx";
+import clsx from "clsx";
 import React from "react";
 
 import styles from "./OrbThrowTooltip.module.scss";
@@ -22,13 +22,35 @@ const OrbThrowTooltip: React.FC<Props> = ({
   }
 
   return (
-    <div
-      className={clsx(
-        `${styles.orbThrowTooltip}`,
-        `orbThrowTooltip`,
-        className,
-      )}
-    ></div>
+    <div className={clsx(styles.orbThrowTooltip, "orbThrowTooltip", className)}>
+      <div className={styles.center}>
+        {/* Four arrowheads radiating outward while group orbits */}
+        <span className={clsx(styles.arrow, styles.arrowA)} />
+        <span className={clsx(styles.arrow, styles.arrowB)} />
+        <span className={clsx(styles.arrow, styles.arrowC)} />
+        <span className={clsx(styles.arrow, styles.arrowD)} />
+
+        {/* Rotating circular text ring around the same center */}
+        <svg
+          className={styles.textRing}
+          viewBox="0 0 100 100"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+        >
+          <defs>
+            <path
+              id="throwTextPath"
+              d="M50,50 m -40,0 a 40,40 0 1,1 80,0 a 40,40 0 1,1 -80,0"
+            />
+          </defs>
+          <text>
+            <textPath xlinkHref="#throwTextPath" startOffset="0%">
+              {"Give a toss! Give a toss! Give a toss! Give a toss! "}
+            </textPath>
+          </text>
+        </svg>
+      </div>
+    </div>
   );
 };
 
