3c88a156d0c1403e6200af51a8c8027ad81e972f
Bradley Baysinger <bhbaysinger@gmail.com>
2025-12-06 20:24:18 -0800

    Move RUM initializer into AppProviders

--- frontend/src/app/providers/AppProviders.tsx
+++ frontend/src/app/providers/AppProviders.tsx
@@ -1,13 +1,21 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import { Provider } from "react-redux";
 
+import { RUMInitializer } from "@/components/RUMInitializer";
 import { store } from "@/store/store";
 
 /**
  * Provider component for app-wide context
  */
 export function AppProviders({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <Suspense fallback={null}>
+        <RUMInitializer />
+      </Suspense>
+      {children}
+    </Provider>
+  );
 }
