7182e67d7189e362f11159d8f9fb37d5e9451cd1
Bradley Baysinger <bhbaysinger@gmail.com>
2025-12-17 09:30:48 -0800

    Obessed with updating packages

--- frontend/src/components/home-page/header-main/ChargedCircle.tsx
+++ frontend/src/components/home-page/header-main/ChargedCircle.tsx
@@ -8,6 +8,7 @@ import SlingerRay from "./SlingerRay";
 
 interface ChargedCircleProps {
   isActive?: boolean;
+  isUnlocked?: boolean;
 }
 
 /**
@@ -15,29 +16,33 @@ interface ChargedCircleProps {
  *
  * @component
  */
-const ChargedCircle: React.FC<ChargedCircleProps> = ({ isActive = false }) => {
+const ChargedCircle: React.FC<ChargedCircleProps> = ({
+  isActive = false,
+  isUnlocked = false,
+}) => {
   // Fixes issue with image data not staying decoded on mobile.
   useScopedImagePreload("/spritesheets/lightning_Layer-Comp-_w480h1098f7.webp");
+  const isVisible = isActive && isUnlocked;
 
   return (
     <div
       className={styles.chargedCircle}
-      style={{ display: isActive ? "unset" : "none" }}
+      style={{ display: isVisible ? "unset" : "none" }}
     >
       <div className={styles.chargedCircleCenter}>
         <div className={styles.chargedCircleRotator}>
           <div className={styles.scaler}>
             <SlingerRay
               className={clsx(styles.bolt, styles.bolt1)}
-              isActive={isActive}
+              isActive={isVisible}
             />
             <SlingerRay
               className={clsx(styles.bolt, styles.bolt2)}
-              isActive={isActive}
+              isActive={isVisible}
             />
             <SlingerRay
               className={clsx(styles.bolt, styles.bolt3)}
-              isActive={isActive}
+              isActive={isVisible}
             />
           </div>
         </div>
