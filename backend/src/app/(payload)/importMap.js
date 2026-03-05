5d23034b99d3f10cfff9db18e3fc6448c05bf48e
Bradley Baysinger <bhbaysinger@gmail.com>
2026-03-04 22:30:26 -0800

    chore(backend): update payload import map

--- backend/src/app/(payload)/importMap.js
+++ backend/src/app/(payload)/importMap.js
@@ -1,7 +1,9 @@
 import { AutoEmailFromUsernameEffect as AutoEmailFromUsernameEffect_1dd4bb9e776155fed557a41f46db5cc2 } from '../../components/payload/AutoEmailFromUsername'
-import { CollectionCards as CollectionCards_ab83ff7e88da8d3530831f296ec4756a } from '@payloadcms/ui/rsc'
+import { S3ClientUploadHandler as S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24 } from '@payloadcms/storage-s3/client'
+import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
 export const importMap = {
   "/components/payload/AutoEmailFromUsername#AutoEmailFromUsernameEffect": AutoEmailFromUsernameEffect_1dd4bb9e776155fed557a41f46db5cc2,
-  "@payloadcms/ui/rsc#CollectionCards": CollectionCards_ab83ff7e88da8d3530831f296ec4756a
+  "@payloadcms/storage-s3/client#S3ClientUploadHandler": S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24,
+  "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
 }
